3005c6f57b6e5d6b936aa5cb5f629797bcc0f557
Zenquan <jomsoucan@gmail.com>
2018-08-23 22:35:48 +0800

    finish hero-detail

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,13 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HeroesComponent } from './heroes/heroes.component';
-import { heroDetailComponent } from './hero-detail/hero-detail.component';
+import { HeroDetailComponent } from './hero-detail/hero-detail.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeroesComponent,
-    heroDetailComponent
+    HeroDetailComponent
   ],
   imports: [
     BrowserModule,
--- src/app/hero-detail/hero-detail.component.spec.ts
+++ src/app/hero-detail/hero-detail.component.spec.ts
@@ -1,20 +1,20 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { heroDetailComponent } from './hero-detail.component';
+import { HeroDetailComponent } from './hero-detail.component';
 
-describe('heroDetailComponent', () => {
-  let component: heroDetailComponent;
-  let fixture: ComponentFixture<heroDetailComponent>;
+describe('HeroDetailComponent', () => {
+  let component: HeroDetailComponent;
+  let fixture: ComponentFixture<HeroDetailComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ heroDetailComponent ]
+      declarations: [ HeroDetailComponent ]
     })
     .compileComponents();
   }));
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(heroDetailComponent);
+    fixture = TestBed.createComponent(HeroDetailComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
